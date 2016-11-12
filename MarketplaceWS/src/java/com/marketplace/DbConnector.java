686ff5f3e89b298d7c6d6b7c89fabc4fe0565cc6
Azzahid <13514095@std.stei.itb.ac.id>
2016-11-12 22:29:12 +0700

    reset

--- MarketplaceWS/src/java/com/marketplace/DbConnector.java
+++ MarketplaceWS/src/java/com/marketplace/DbConnector.java
@@ -5,8 +5,10 @@
  */
 package com.marketplace;
 
-import java.sql.*;
-
+import com.mysql.jdbc.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -56,6 +58,9 @@ public class DbConnector {
             }else{
                 return null;
             }
+            if(count==0){
+                return null;
+            }
             es = new Product[count];
             query = ("SELECT * FROM product");
             rs = st.executeQuery(query);
@@ -75,7 +80,7 @@ public class DbConnector {
                 es[i].setNamaProduk(rs.getString("namaProduk"));
                 es[i].setDescription(rs.getString("description"));
                 es[i].setPrice(rs.getString("price"));
-                es[i].setPhotoUrl(rs.getBytes("photo_Url"));
+                es[i].setPhotoUrl(null);
                 es[i].setCreatedAt(rs.getDate("created_At"));
                 es[i].setImageType(rs.getString("image_Type"));
                 es[i].setImageName(rs.getString("image_Name"));
@@ -149,6 +154,9 @@ public class DbConnector {
             }else{
                 return null;
             }
+            if(count==0){
+                return null;
+            }
             es = new Product[count];
             rs = st.executeQuery(query1);
             while(rs.next()){
@@ -157,17 +165,73 @@ public class DbConnector {
                 es[i].setNamaProduk(rs.getString("namaProduk"));
                 es[i].setDescription(rs.getString("description"));
                 es[i].setPrice(rs.getString("price"));
-                es[i].setPhotoUrl(rs.getBytes("photo_Url"));
+                es[i].setPhotoUrl(null);
                 es[i].setCreatedAt(rs.getDate("created_at"));
                 es[i].setImageType(rs.getString("image_Type"));
                 es[i].setImageName(rs.getString("image_Name"));
                 es[i].setUserId(rs.getInt("user_Id"));
                 i++;
             }
-        }catch(Exception ex){
+        }catch(SQLException ex){
             System.out.println("Result: "+ex);
         }
         
         return es;
     }
+    
+    public Purchase[] getProductPurchase(Integer userid, Integer pilihan){
+        Purchase[] es = null;
+        String query;
+        String query1;
+        if(pilihan  == 1){
+            query = ("SELECT COUNT(*) AS total FROM purchase WHERE purchase.buyer_id = '"+userid+"'");
+            query1 = ("SELECT * FROM purchase WHERE purchase.buyer_id = '"+userid+"' ORDER BY created_at DESC");
+        }else{
+            query = ("SELECT COUNT(*) AS total FROM purchase WHERE purchase.seller_id = '"+userid+"'");
+            query1 = ("SELECT * FROM purchase WHERE purchase.seller_id = '"+userid+"' ORDER BY created_at DESC");
+        }
+        try{
+            int count;
+            int i = 0;
+            rs = st.executeQuery(query);
+            if(rs.next()){
+                count = rs.getInt("total");
+            }else{
+                return null;
+            }
+            if(count==0){
+                return null;
+            }
+            es = new Purchase[count];
+            rs = st.executeQuery(query1);
+            while (rs.next()) {
+                /*private Integer purchaseId;
+            */
+                es[i] = new Purchase();
+                es[i].setPurchaseId(rs.getInt("purchase_id"));
+                es[i].setBuyerId(rs.getInt("buyer_id"));
+                es[i].setProductId(rs.getInt("product_id"));
+                es[i].setConsignee("consignee");
+                es[i].setFulladdress(rs.getString("fulladdress"));
+                es[i].setQuantity(rs.getInt(6));
+                es[i].setCreditcardnumber(rs.getString(7));
+                es[i].setPostalcode(rs.getString(8));
+                es[i].setPhonenumber(rs.getString(9));
+                es[i].setCreatedAt(rs.getDate(10));
+                es[i].setCardVerification(rs.getString(11));
+                es[i].setProductName(rs.getString(12));
+                es[i].setProductDescription(rs.getString(13));
+                es[i].setProductPrice(rs.getString(14));
+                es[i].setProductPhotourl(null);
+                es[i].setSellerId(rs.getInt(16));
+                es[i].setImageType(rs.getString(17));
+                i++;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Result: " + ex);
+        }
+        
+        return es;
+    }
+    
 }
--- StackExchangeClient/web/purchases.jsp
+++ StackExchangeClient/web/purchases.jsp
@@ -4,6 +4,9 @@
     Author     : user-BL
 --%>
 
+<%@page import="java.util.Date"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="com.marketplace.*"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -23,7 +26,56 @@
         <!-- search form -->
         <!--BagianProduk rencananya pake PHP di echo satu-satu-->
         <div id = "sales">
-                
+                   <%-- start web service invocation --%><hr/>
+    <%
+    try {
+	com.marketplace.Marketplace_Service service = new com.marketplace.Marketplace_Service();
+	com.marketplace.Marketplace port = service.getMarketplacePort();
+	 // TODO initialize WS operation arguments here
+	int userid = 7;
+	// TODO process result here
+	java.util.List<com.marketplace.Purchase> result = port.getProductPurchase(userid);
+        SimpleDateFormat ft = new SimpleDateFormat("E, dd M yyyy");
+        SimpleDateFormat tt = new SimpleDateFormat("'at 'HH.mm");
+	if(result != null && result.size()>0){
+            for(int i =0; i<result.size();i++){
+                Purchase temp = result.get(i);
+                Date datetemp = temp.getCreatedAt().toGregorianCalendar().getTime();
+                out.println("<div class = 'product'>");
+                    out.println("<span class='product-date'>"+ft.format(datetemp)+"</span><br />");
+                    out.println("<span class = 'product-time'>"+tt.format(datetemp)+"</span>");
+                    out.println("<hr />");
+                    out.println("<img src='' alt='product-image' width='100px' height='100px'>");
+                    out.println("<div class='product-center-description'>");
+                        out.println("<span class='product-name'>"+temp.getProductName()+"</span><br />");
+                        out.println("<span class= 'product-price'>IDR "+Integer.parseInt(temp.getProductPrice())*temp.getQuantity()+"</span><br />");
+                        out.println("<span class='product-price'>"+temp.getQuantity()+" pcs</span><br />");
+                        out.println( "<span class='product-price'>@IDR "+temp.getProductPrice()+"</span><br />");
+                    out.println("</div>");
+                    out.println("<div class='product-right-description'>");
+                        out.println("<div class='margin-top'>");
+                            out.println("<span class='product-desc'>Delivery to </span>");
+                            out.println("<span class='product-desc'><b>"+temp.getConsignee()+"</b></span><br />");
+                            out.println("<span class='product-desc'>"+temp.getFulladdress()+"</span><br />");
+                            out.println("<span class='product-desc'>"+temp.getPostalcode()+"</span><br />");
+                            out.println("<span class='product-desc'>"+temp.getPhonenumber()+"</span><br />");
+                        out.println("</div>");
+                    out.println("</div>");
+                    out.println("<div class='product-center-description margin-top'>");
+                        out.println("<span class='product-bottom-desc '>bought from ");
+                        out.println("<b>"+temp.getSellerId()+"</b><span>");
+                    out.println("</div>");
+                out.println("</div>");
+            }
+        }else{
+            out.println("You haven't purchased anything");
+        }
+
+    } catch (Exception ex) {
+	out.println("Error:"+ex);
+    }
+    %>
+    <%-- end web service invocation --%><hr/> 
         </div>
         <script type="text/javascript" src="js/catalog.js"></script>
     </body>
